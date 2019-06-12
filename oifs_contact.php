a4f23ef3d6e480ad2073e81bdc79cc94e362da80
Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>
2019-06-12 09:51:44 +0100

    Minor tweaks

--- oifs_contact.php
+++ oifs_contact.php
@@ -39,11 +39,11 @@ echo '<hr>';
 <h1>New Collaboration Enquiry Form</h1>
 <form name="contactform" method="post" action="send_form_email.php">
 <table width="100%" border="0" style="border:none;">
-<tr class="nohover"><td Width=22%>First Name:</td><td width=78%><input type="text" name="FirstName" size="20"></td></tr>
+<tr class="nohover"><td Width=10%>First Name:</td><td width=90%><input type="text" name="FirstName" size="20"></td></tr>
 <tr class="nohover"><td>Last Name:</td><td><input type="text" id="LastName" name="LastName" size="20"></td></tr>
 <tr class="nohover"><td>Affiliation:</td><td><input type="text" id="Affiliation" name="Affiliation" size="50"></td></tr>
 <tr class="nohover"><td>E-mail:</td><td><input type="text" id="email" name="email" size="50"></td></tr>        
-<tr class="nohover"><td>Message:</td><td><textarea id="Message" name="Message" rows="3" cols="80"></textarea></td></tr>
+<tr class="nohover"><td>Message:</td><td><textarea id="Message" name="Message" rows="10" cols="90"></textarea></td></tr>
 </table>
 <input class="button" type="submit" name="submit" value="Send" />
 </form>
